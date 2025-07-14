8ec35177d389adbcc5cb7f20142b4551e8b137f3
vikash kumar <47270358+kvikash668@users.noreply.github.com>
2025-07-15 01:11:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+terraform {
+  cloud {
+
+    organization = "TERRAFORM_LEARN668"
+
+    workspaces {
+      name = "LEARN668_CLI"
+    }
+  }
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-west-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "peer" {
+  cidr_block = "10.1.0.0/16"
+}
